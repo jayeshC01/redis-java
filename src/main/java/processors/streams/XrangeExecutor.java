07cb940a4c17e98da138eb43f0943457b7978103
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2025-09-12 02:04:46 +0530

    Support for XRANGE using - and +

--- src/main/java/processors/streams/XrangeExecutor.java
+++ src/main/java/processors/streams/XrangeExecutor.java
@@ -16,38 +16,49 @@ public class XrangeExecutor implements CommandExecutor {
   public String execute(RespCommand cmd) {
     try {
       validateCommand(cmd);
-    } catch (IllegalArgumentException e) {
+
+      String key = cmd.getKey();
+      DataStoreValue value = DataStore.get(key);
+      if (value == null || value.getAsStream().isEmpty()) {
+        return RespUtility.serializeResponse(Collections.emptyList());
+      }
+
+      ConcurrentNavigableMap<String, Map<String, String>> existingData = value.getAsStream();
+      String start = cmd.getArgs().get(1).equals("-") ? existingData.firstKey() : cmd.getArgs().get(1);
+      String end = cmd.getArgs().get(2).equals("+") ? existingData.lastKey() : cmd.getArgs().get(2);
+      ConcurrentNavigableMap<String, Map<String, String>> output =
+          existingData.subMap(start, true, end, true);
+      if (output.isEmpty()) {
+        return RespUtility.serializeResponse(Collections.emptyList());
+      }
+
+      return RespUtility.serializeStreamResponse(output);
+    } catch (IllegalArgumentException | IllegalStateException e) {
       return RespUtility.buildErrorResponse(e.getMessage());
     }
-    String key = cmd.getKey();
-    DataStoreValue value = DataStore.get(key);
-    if (value == null || value.getAsStream().isEmpty()) {
-      return RespUtility.serializeResponse(Collections.emptyList());
-    }
-    String start = cmd.getArgs().get(1);
-    String end = cmd.getArgs().get(2);
-    ConcurrentNavigableMap<String, Map<String, String>> output = value.getAsStream().subMap(start, true, end, true);
-    if(output.isEmpty()) {
-      return RespUtility.serializeResponse(Collections.emptyList());
-    }
-    return RespUtility.serializeStreamResponse(output);
   }
 
   private void validateCommand(RespCommand cmd) {
     if (cmd.getKey() == null) {
       throw new IllegalArgumentException("Invalid Args - Missing Key");
     }
-    if(cmd.getArgsSize() != 3) {
+    if (cmd.getArgsSize() != 3) {
       throw new IllegalArgumentException("Invalid Args: Missing Start or End");
     }
 
-    String[] start = cmd.getArgs().get(1).split("-");
-    String[] end = cmd.getArgs().get(2).split("-");
-    if(Long.parseLong(start[0]) == Long.parseLong(end[0])) {
-      if(Long.parseLong(start[1]) > Long.parseLong(end[1])) {
+    String start = cmd.getArgs().get(1);
+    String end = cmd.getArgs().get(2);
+    if (start.equals("-") || end.equals("+")) {
+      return;
+    }
+
+    String[] startRange = start.split("-");
+    String[] endRange = end.split("-");
+    if (Long.parseLong(startRange[0]) == Long.parseLong(endRange[0])) {
+      if (Long.parseLong(startRange[1]) > Long.parseLong(endRange[1])) {
         throw new IllegalArgumentException("Start greater than end");
       }
-    } else if (Long.parseLong(start[0]) > Long.parseLong(end[0])) {
+    } else if (Long.parseLong(startRange[0]) > Long.parseLong(endRange[0])) {
       throw new IllegalArgumentException("Start greater than end");
     }
   }
