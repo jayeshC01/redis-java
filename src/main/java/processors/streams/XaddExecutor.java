d6b423ee82be436e178127c813dfdc2b19fa9d57
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2025-09-11 12:29:11 +0530

    Support for stream ID generation

--- src/main/java/processors/streams/XaddExecutor.java
+++ src/main/java/processors/streams/XaddExecutor.java
@@ -16,36 +16,44 @@ public class XaddExecutor implements CommandExecutor {
 
   @Override
   public String execute(RespCommand cmd) {
-    String key = cmd.getKey();
-    List<String> args = cmd.getArgs().subList(1, cmd.getArgsSize());
-    String entryID = args.getFirst();
-    Map<String, String> entryValue = new HashMap<>();
-
-    for (int i = 1; i < args.size(); i += 2) {
-      entryValue.put(args.get(i), args.get(i + 1));
-    }
-
-    DataStoreValue existingData = DataStore.get(key);
-    ConcurrentNavigableMap<String, Map<String, String>> stream;
-    if (existingData == null) {
-      stream = new ConcurrentSkipListMap<>();
-      DataStore.put(key, new DataStoreValue(stream));
-    } else {
-      try {
-        stream = existingData.getAsStream();
-      } catch (Exception e) {
-        return RespUtility.buildErrorResponse(e.getMessage());
-      }
-    }
-
     try {
-      validateStreamID(entryID, stream);
-    } catch(IllegalArgumentException e) {
+      validateCmd(cmd);
+      String key = cmd.getKey();
+      List<String> args = cmd.getArgs().subList(1, cmd.getArgsSize());
+      String entryID = args.getFirst();
+      Map<String, String> entryValue = new HashMap<>();
+
+      for (int i = 1; i < args.size(); i += 2) {
+        entryValue.put(args.get(i), args.get(i + 1));
+      }
+
+      DataStoreValue existingData = DataStore.get(key);
+      ConcurrentNavigableMap<String, Map<String, String>> stream;
+      if (existingData == null) {
+        stream = new ConcurrentSkipListMap<>();
+        DataStore.put(key, new DataStoreValue(stream));
+      } else {
+        stream = existingData.getAsStream();
+      }
+
+      String generatedID = resolveStreamID(entryID, stream);
+      stream.put(generatedID, entryValue);
+
+      return RespUtility.serializeResponse(generatedID);
+    } catch(IllegalArgumentException | IllegalStateException e) {
       return RespUtility.buildErrorResponse(e.getMessage());
     }
+  }
 
-    stream.put(entryID, entryValue);
-    return RespUtility.serializeResponse(entryID);
+  private void validateCmd(RespCommand cmd) {
+    if (cmd.getArgsSize() < 4) {
+      throw new IllegalArgumentException("Wrong number of arguments for XADD command");
+    }
+
+    List<String> fieldValues = cmd.getArgs().subList(2, cmd.getArgsSize());
+    if (fieldValues.size() % 2 != 0) {
+      throw new IllegalArgumentException("XADD requires field-value pairs");
+    }
   }
 
   private void validateStreamID(String entryID, ConcurrentNavigableMap<String, Map<String, String>> stream) {
@@ -78,5 +86,48 @@ public class XaddExecutor implements CommandExecutor {
     long oldSeq = Long.parseLong(oldParts[1]);
 
     return (newMs > oldMs) || (newMs == oldMs && newSeq > oldSeq);
+  }
+
+  private String resolveStreamID(String entryID, ConcurrentNavigableMap<String, Map<String, String>> stream) {
+    long ms, seq;
+    if (entryID.equals("*")) {
+      ms = System.currentTimeMillis();
+      seq = 0;
+      if (!stream.isEmpty()) {
+        String lastId = stream.lastKey();
+        String[] parts = lastId.split("-");
+        long lastMs = Long.parseLong(parts[0]);
+        long lastSeq = Long.parseLong(parts[1]);
+
+        if (lastMs == ms) {
+          seq = lastSeq + 1;
+        }
+      }
+
+      return ms + "-" + seq;
+    }
+
+    if (entryID.endsWith("-*")) {
+      ms = Long.parseLong(entryID.split("-")[0]);
+      seq = ms == 0 ? 1 : 0;
+
+      if (!stream.isEmpty()) {
+        String lastId = stream.lastKey();
+        String[] parts = lastId.split("-");
+        long lastMs = Long.parseLong(parts[0]);
+        long lastSeq = Long.parseLong(parts[1]);
+
+        if (lastMs == ms) {
+          seq = lastSeq + 1;
+        }
+      }
+
+      String newId = ms + "-" + seq;
+      validateStreamID(newId, stream);
+      return newId;
+    }
+
+    validateStreamID(entryID, stream);
+    return entryID;
   }
 }
