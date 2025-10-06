9e0c97b7e6180c1d91f3e99bd3615c34a08639ad
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2025-10-07 01:30:05 +0530

    Enhance INFO command to include optional replication fields

--- src/main/java/config/ConfigProcessor.java
+++ src/main/java/config/ConfigProcessor.java
@@ -7,26 +7,34 @@ public class ConfigProcessor {
   public static final Map<String, String> configs = new ConcurrentHashMap<>();
 
   public static void processAndStoreConfig(String[] args) {
-    for(int i = 0; i<args.length; i++) {
-      if(args[i].startsWith("--")){
-        if(args[i].substring(2).equalsIgnoreCase("replicaOf")) {
-          configs.put("role", "slave");
-          i++;
-          System.out.println("Testing "+args[i]);
-          if(args[i].equalsIgnoreCase("localhost")) {
-            configs.put("masterHost", "127.0.0.1");
+    for (int i = 0; i < args.length; i++) {
+      if (!args[i].startsWith("--")) continue;
+
+      String key = args[i].substring(2);
+      if (key.equalsIgnoreCase("replicaOf")) {
+        configs.put("role", "slave");
+        if (++i < args.length) {
+          String[] replicaInfo = args[i].split("\\s+");
+          String host = replicaInfo[0].equalsIgnoreCase("localhost") ? "127.0.0.1" : replicaInfo[0];
+          configs.put("masterHost", host);
+          if (replicaInfo.length > 1) {
+            configs.put("masterPort", replicaInfo[1]);
           }
-          String[] replicaString = args[i].split("\\s+");
-          configs.put("masterHost", replicaString[0]);
-          configs.put("masterPort", replicaString[1]);
-          continue;
         }
-        configs.put(args[i].substring(2), args[i+1]);
-        i++;
+      } else {
+        if (i + 1 < args.length) {
+          configs.put(key, args[++i]);
+        }
       }
     }
-    if(!configs.containsKey("role")) {
-      configs.put("role", "master");
+    configs.putIfAbsent("role", "master"); // Default role is master
+    configureMasterSlaveDefaults();
+  }
+
+  private static void configureMasterSlaveDefaults() {
+    if ("master".equalsIgnoreCase(configs.get("role"))) {
+      configs.put("master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
+      configs.put("master_repl_offset", "0");
     }
   }
 }
--- src/main/java/processors/replication/InfoExecutor.java
+++ src/main/java/processors/replication/InfoExecutor.java
@@ -1,20 +1,39 @@
 package processors.replication;
 
-import config.ConfigProcessor;
 import models.RespCommand;
 import processors.CommandExecutor;
 import utility.RespUtility;
+import static config.ConfigProcessor.configs;
 
 public class InfoExecutor implements CommandExecutor {
 
   @Override
   public String execute(RespCommand cmd) {
     if (cmd.getArgs().size() != 1) {
-      return "-ERR wrong number of arguments for 'info' command\r\n";
+      return RespUtility.buildErrorResponse("wrong number of arguments for 'info' command");
     }
+    //TODO: Implement other sections of INFO command
     if (!cmd.getArgs().getFirst().equalsIgnoreCase("replication")) {
-      return "-ERR Unsupported INFO section\r\n";
+      return RespUtility.buildErrorResponse("Unsupported INFO section");
     }
-    return RespUtility.serializeResponse("role:" + ConfigProcessor.configs.get("role") + "\r\n");
+    StringBuilder responseBuilder = new StringBuilder();
+    responseBuilder.append("# Replication\r\n");
+    responseBuilder.append("role:").append(configs.get("role")).append("\r\n");
+
+    if (configs.containsKey("master_replid")) {
+      responseBuilder.append("master_replid:").append(configs.get("master_replid")).append("\r\n");
+    }
+
+    if (configs.containsKey("master_repl_offset")) {
+      responseBuilder.append("master_repl_offset:").append(configs.get("master_repl_offset")).append("\r\n");
+    }
+    if ("slave".equalsIgnoreCase(configs.get("role"))) {
+      responseBuilder.append("master_host:").append(configs.get("masterHost")).append("\r\n");
+      responseBuilder.append("master_port:").append(configs.get("masterPort")).append("\r\n");
+      responseBuilder.append("master_link_status:up\r\n");
+    }
+
+    String response = responseBuilder.toString();
+    return RespUtility.serializeResponse(response);
   }
 }
