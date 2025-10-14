742ec570727467cda8b18abdd7131fa1b60741cd
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2025-10-14 17:36:24 +0530

    Refactored code

--- src/main/java/server/ReplicationManager.java
+++ src/main/java/server/ReplicationManager.java
@@ -21,54 +21,75 @@ public class ReplicationManager {
     int masterPort = !masterPortStr.isEmpty() ? Integer.parseInt(masterPortStr) : DEFAULT_PORT;
     System.out.println("[REPL] Starting replication with master " + masterHost + ":" + masterPort);
 
-    Thread t = new Thread(() -> {
-      try (Socket socket = new Socket(masterHost, masterPort);
-           BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-           BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-
-        System.out.println("[REPL] Connected to master " + masterHost + ":" + masterPort);
-        System.out.println("[REPL] Starting replication handshake...");
-
-        RespCommand pingCommand = new RespCommand("PING");
-        writer.write(RespUtility.serializeCommand(pingCommand));
-        writer.flush();
-        String pingResponse = reader.readLine();
-        if (!pingResponse.equals("+PONG")) {
-          System.out.println("[REPL] Error: Unexpected PING response from master: " + pingResponse);
-          return;
-        }
-
-        RespCommand replConf1 = new RespCommand("REPLCONF",
-            List.of("listening-port", String.valueOf(configs.get(PORT))));
-        writer.write(RespUtility.serializeCommand(replConf1));
-        writer.flush();
-        String response2 = reader.readLine();
-        if (!response2.equals("+OK")) {
-          System.out.println("[REPL] Error: REPLCONF listening-port failed: " + response2);
-          return;
-        }
-
-        //TODO: Hardcoded for now, will be dynamic later
-        RespCommand replConf2 = new RespCommand("REPLCONF",
-            List.of("capa", "psync2"));
-        writer.write(RespUtility.serializeCommand(replConf2));
-        writer.flush();
-        String response3 = reader.readLine();
-        if (!response3.equals("+OK")) {
-          System.out.println("[REPL] Error: REPLCONF capa failed: " + response3);
-          return;
-        }
-
-        RespCommand psyncCommand = new RespCommand("PSYNC", List.of("?", "-1"));
-        writer.write(RespUtility.serializeCommand(psyncCommand));
-        writer.flush();
-      } catch (IOException e) {
-        System.out.println("[REPL] Error during replication handshake: " + e.getMessage());
-      }
-    }, "replication-slave-handshake");
-
+    Thread t = new Thread(() ->
+        performSlaveReplicationHandshake(masterHost, masterPort),
+        "replication-slave-handshake");
     t.setDaemon(true);
     t.start();
+  }
+
+  private static void performSlaveReplicationHandshake(String masterHost, int masterPort) {
+    try (Socket socket = new Socket(masterHost, masterPort);
+         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+      System.out.println("[REPL] Connected to master " + masterHost + ":" + masterPort);
+      System.out.println("[REPL] Starting replication handshake...");
+      Boolean handShakePerform = initiateHandshake(reader, writer);
+      if(handShakePerform) {
+        System.out.println("[REPL] Handshake part completed, waiting for RDB file...");
+      } else {
+        System.out.println("[REPL] Handshake failed, Retrying...");
+      }
+    } catch (IOException e) {
+      System.out.println("[REPL] Error during replication handshake: " + e.getMessage());
+    }
+  }
+
+
+  private static Boolean initiateHandshake(BufferedReader reader, BufferedWriter writer) throws IOException {
+    RespCommand pingCommand = new RespCommand("PING");
+    writer.write(RespUtility.serializeCommand(pingCommand));
+    writer.flush();
+    String pingResponse = reader.readLine();
+    if (!pingResponse.equals("+PONG")) {
+      System.out.println("[REPL] Error: Unexpected PING response from master: " + pingResponse);
+      return false;
+    }
+
+    //Handshake Part 2.A: REPLCONF
+    RespCommand replConf1 = new RespCommand("REPLCONF",
+        List.of("listening-port", String.valueOf(configs.get(PORT))));
+    writer.write(RespUtility.serializeCommand(replConf1));
+    writer.flush();
+    String response2 = reader.readLine();
+    if (!response2.equals("+OK")) {
+      System.out.println("[REPL] Error: REPLCONF listening-port failed: " + response2);
+      return false;
+    }
+
+    //Handshake Part 2.B: REPLCONF CAPA
+    //TODO: Hardcoded for now, will be dynamic later
+    RespCommand replConf2 = new RespCommand("REPLCONF",
+        List.of("capa", "psync2"));
+    writer.write(RespUtility.serializeCommand(replConf2));
+    writer.flush();
+    String response3 = reader.readLine();
+    if (!response3.equals("+OK")) {
+      System.out.println("[REPL] Error: REPLCONF capa failed: " + response3);
+      return false;
+    }
+
+    //Handshake Part 3: PSYNC
+    RespCommand psyncCommand = new RespCommand("PSYNC", List.of("?", "-1"));
+    writer.write(RespUtility.serializeCommand(psyncCommand));
+    writer.flush();
+    String response4 = reader.readLine();
+    if (!response4.startsWith("+FULLRESYNC")) {
+      System.out.println("[REPL] Error: Invalid FULLRESYNC response: " + response4);
+      return false;
+    }
+    return true;
   }
 }
 
